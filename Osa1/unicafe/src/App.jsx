d805e254fddba59fd668c7b57a55485d73df534f
kiirona20 <allankiironen7@gmail.com>
2024-07-07 13:52:52 +0300

    tehtävä 1.7

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -1,29 +1,79 @@
 import { useState } from 'react'
 
+const DisplayStatistics = ({good,neutral,bad,total,average,positive}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive}%</p>
+
+
+    </div>
+  )
+}
+
 
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [reviews, setReviews] = useState({
+    good:0, neutral:0, bad:0, total:0, score:0
+  })
+  const [statistics, setStatistics] = useState({
+    average:0, positivePercentage:0
+  })
+
+
 
   const handleGoodClick = () => {
-    console.log("This is before the click", good)
-    const updatedScore = good + 1
-    setGood(updatedScore)
-    console.log("This is after the click", updatedScore)
-
+    const newReviews = {
+      ...reviews,
+      good: reviews.good + 1,
+      total: reviews.total + 1,
+      score: reviews.score + 1
+    }
+    setReviews(newReviews)
+    const updatedStatistics = {
+      average: newReviews.score/newReviews.total,
+      positivePercentage: newReviews.good/newReviews.total * 100
+    }
+    setStatistics(updatedStatistics)
   }
 
   const handleNeutralClick = () => {
-    const updatedScore = neutral + 1
-    setNeutral(updatedScore)
+    const newReviews = {
+      ...reviews,
+      neutral: reviews.neutral + 1,
+      total: reviews.total + 1
+    }
+    setReviews(newReviews)
+    const updatedStatistics = {
+      average: newReviews.score/newReviews.total,
+      positivePercentage: newReviews.good/newReviews.total * 100
+    }
+    setStatistics(updatedStatistics)
+
   }
 
   const handleBadClick = () => {
-    const updatedScore = bad + 1
-    setBad(updatedScore)
+    const newReviews = {
+      ...reviews,
+      bad: reviews.bad + 1,
+      total: reviews.total + 1,
+      score: reviews.score - 1
+    }
+    setReviews(newReviews)
+    const updatedStatistics = {
+      average: newReviews.score/newReviews.total,
+      positivePercentage: newReviews.good/newReviews.total * 100
+    }
+    setStatistics(updatedStatistics)
   }
+
+ 
 
 
 
@@ -33,10 +83,8 @@ const App = () => {
         <button onClick={handleGoodClick}>Good</button>
         <button onClick={handleNeutralClick}>Neutral</button>    
         <button onClick={handleBadClick}>Bad</button>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+        <DisplayStatistics good={reviews.good} neutral={reviews.neutral} bad={reviews.bad} total={reviews.total} average={statistics.average} positive={statistics.positivePercentage}/>
+
 
 
     </div>
