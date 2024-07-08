b302a0fe8bf0ba4a66ac5d5141374d61105105f0
kiirona20 <allankiironen7@gmail.com>
2024-07-08 23:44:59 +0300

    Tehtävä 2.9*

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'
-     }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
 const Person = ({person}) => {
   return(
   <p>{person.name} {person.number}</p>
   )
 }
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
 
 const addPerson = (event) =>{
   event.preventDefault()
@@ -37,13 +38,19 @@ const handlePerson = (event) => {
 }
 
 const handleNumber = (event) => {
-  console.log(event.target.value)
   setNewNumber(event.target.value)
 }
 
+const handleFilter = (event) => {
+  console.log(event.target.value)
+  
+}
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input value={newFilter}
+      onChange={handleFilter}/> 
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}
@@ -53,14 +60,18 @@ const handleNumber = (event) => {
           number: <input value={newNumber}
           onChange={handleNumber}
           />
-
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
+      
+
+
+
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person =>
       <Person key={person.name} person={person}/>
       )}
     </div>
