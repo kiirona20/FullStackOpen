b59fcc350dd0d0f2318f98386a1c411e782c3b5c
kiirona20 <allankiironen7@gmail.com>
2024-07-12 22:24:24 +0300

    4.5*

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -6,13 +6,19 @@ const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
         return sum + item.likes
     }
-
     return blogs.reduce(reducer,0)
-    
-
 }
 
+const favoriteBlog = (blogs) => {
+    let index = 0
+    for(let i=0; i<blogs.length; i++){
+        if (blogs[i].likes>blogs[index].likes){
+            index=i
+        }
+    }
+    return blogs[index];
+}
 
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
