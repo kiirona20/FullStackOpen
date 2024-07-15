5e8310759eae64c59650233922b7b0da69d65090
kiirona20 <allankiironen7@gmail.com>
2024-07-15 13:32:37 +0300

    5.2: blogilistan frontend

--- Osa5/bloglist-frontend/src/App.jsx
+++ Osa5/bloglist-frontend/src/App.jsx
@@ -19,6 +19,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])  
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -26,6 +35,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
+
       console.log(user)
       setUser(user)
       setUsername('')
@@ -38,6 +52,15 @@ const App = () => {
     }
   
   }
+
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+
+
+
+  }
+
   if (user === null) {
     return (
       <div>
@@ -74,7 +97,8 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
