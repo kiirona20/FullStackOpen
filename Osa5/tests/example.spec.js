e1e2a47320444c11f27ced3e001d12ed59663d62
kiirona20 <allankiironen7@gmail.com>
2024-07-17 00:01:14 +0300

    5.17

---
+++ Osa5/tests/example.spec.js
@@ -0,0 +1,26 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Note app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('Log in to application')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    expect(page.getByRole('button', { name: 'log in' })).toBeVisible
+    expect(page.getByRole('textbox')).toHaveCount(2)
+
+  })
+})
